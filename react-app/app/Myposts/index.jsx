a60816c3cfd699699717524da4aaa185ad10fc61
NITHYA-B-A28 <nithyaba44@gmail.com>
2026-02-23 17:38:53 +0530

    fixed conflict

--- react-app/app/Myposts/index.jsx
+++ react-app/app/Myposts/index.jsx
@@ -1,22 +1,14 @@
 // eslint-disable-next-line import/extensions
 import { createRoot } from 'react-dom/client';
 // eslint-disable-next-line no-unused-vars
-<<<<<<<< HEAD:react-app/app/upcomingevents/index.jsx
-import UpcomingEvents from './components/app.jsx';
-========
 import MyPosts from './components/app.jsx';
->>>>>>>> develop:react-app/app/Myposts/index.jsx
 import './styles/index.css';
 
 export async function decorateBlock(block) {
   const root = createRoot(block);
 
   root.render(
-<<<<<<<< HEAD:react-app/app/upcomingevents/index.jsx
-    <UpcomingEvents />,
-========
     <MyPosts />,
->>>>>>>> develop:react-app/app/Myposts/index.jsx
   );
 }
 
