c6ce9de9aaa43aa06a9fb0c9a23051f0f67a5d71
Sudeep Laxmish Naik <slaxmish@adobe.com>
2026-02-23 16:53:24 +0530

    Content

--- react-app/app/banner/components/app.jsx
+++
@@ -1,9 +0,0 @@
-const Banner = () => (
-  <div className="banner">
-    <h2>Welcome to Our Banner</h2>
-    <p>This is a simple banner component built with React.</p>
-    <button type="button">Learn More</button>
-  </div>
-);
-
-export default Banner;
